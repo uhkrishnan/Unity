6fe54d1748249c929874c36c0b1e86340ba78433
Harikrishnan U <68114886+uhkrishnan@users.noreply.github.com>
2021-05-04 09:47:43 +0530

    CoinFlip-Production-20210502

---
+++ CoinFlip-Production-20210502/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void Start()
+    {
+        this.Wait(3f, () =>
+        {
+            SceneManager.LoadScene("GameScene");
+        });
+    }
+}
